5a1919c05e520fab6141f94e6a1d98db7e0903d0
HidekazuFutagami-Takeda <80449453+HidekazuFutagami-Takeda@users.noreply.github.com>
2025-02-20 19:58:02 +0900

    消えたもの復活

---
+++ src/jp/co/takeda/rdm/action/ND307Action.java
@@ -0,0 +1,160 @@
+/*
+ * ALL RIGHTS RESERVED,
+ * COPYRIGHT (C) 2013, HITACHI SOLUTIONS, Ltd.
+ */
+//## AutomaticGeneration
+package jp.co.takeda.rdm.action;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.opensymphony.xwork2.interceptor.annotations.Before;
+import com.opensymphony.xwork2.interceptor.annotations.BeforeResult;
+import com.opensymphony.xwork2.interceptor.annotations.InputConfig;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.springframework.context.annotation.Scope;
+
+import jp.co.takeda.rdm.common.BaseAction;
+import jp.co.takeda.rdm.common.BaseDTO;
+import jp.co.takeda.rdm.common.BaseInfoHolder;
+import jp.co.takeda.rdm.service.ND307Service;
+import jp.co.takeda.rdm.util.AppConstant;
+import jp.co.takeda.rdm.util.JkrConstantsData;
+import jp.co.takeda.rdm.common.LoginInfo;
+import jp.co.takeda.rdm.dto.ND307DTO;
+
+/**
+ * Actionクラス
+ * @generated
+ */
+@Named("nD307Action")
+@Scope("request")
+public class ND307Action extends BaseAction<ND307DTO> {
+
+    /**
+     * シリアルバージョンID
+     * @generated
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Serviceオブジェクト
+     * @generated
+     */
+    @Inject
+    private ND307Service nD307Service;
+
+    // START UOC
+
+    // END UOC
+
+    /**
+     * コンストラクタ
+     * @generated
+     */
+    public ND307Action() {
+        dto = new ND307DTO();
+    }
+
+    /**
+     * イベント毎のメソッド実行前に共通後処理。<br/>
+     * 但し、validationエラー時は実行されない。
+     * @return 遷移先（nullの場合、イベントのメソッドを呼び出す）
+     * @customizable
+     */
+    @Before
+    public String before() throws Exception {
+        // START UOC
+        return null;
+        // END UOC
+    }
+
+    /**
+     * イベント毎のメソッド実行前に共通後処理。<br/>
+     * 但し、validationエラー時は実行されない。
+     * @customizable
+     */
+    @BeforeResult
+    public void beforeResult() throws Exception {
+        // START UOC
+
+        // END UOC
+    }
+
+    /**
+     * 業務処理
+     * @customizable
+     */
+    public String init() throws Exception {
+        initSetup();
+        // F層呼び出し
+        BaseDTO outdto = nD307Service.init(dto);
+        return initNext(outdto);
+    }
+
+    /**
+     * 前処理
+     * @customizable
+     */
+    protected void initSetup() throws Exception {
+        // START UOC
+    	LoginInfo loginInfo = (LoginInfo) BaseInfoHolder.getUserInfo();
+
+        String preScreenId = loginInfo.getPreScreenId();
+        dto.setPreScreenId(preScreenId);
+
+		String title = "ND307_医療機関への異動 - 申請内容確認";
+
+		dto.setTitle(title);
+        // END UOC
+    }
+
+    /**
+     * 後処理
+     * @customizable
+     */
+    protected String initNext(BaseDTO outdto) throws Exception {
+        // START UOC
+        // 検索条件をセッションに格納する（更新やソートリンク押下時に使用）
+        sessionMap.put(AppConstant.SESKEY_ND307_SEARCHKEY, outdto);
+        // END UOC
+        setNextDTO(outdto);
+        return outdto.getForward();
+    }
+
+    /**
+     * 業務処理
+     * @customizable
+     */
+    @InputConfig(methodName="validationError")
+    public String register() throws Exception {
+        registerSetup();
+        // F層呼び出し
+        BaseDTO outdto = nD307Service.register(dto);
+        return registerNext(outdto);
+    }
+
+    /**
+     * 前処理
+     * @customizable
+     */
+    protected void registerSetup() throws Exception {
+        // START UOC
+        // END UOC
+    }
+
+    /**
+     * 後処理
+     * @customizable
+     */
+    protected String registerNext(BaseDTO outdto) throws Exception {
+        // START UOC
+        // END UOC
+    	LoginInfo loginInfo = (LoginInfo)BaseInfoHolder.getUserInfo();
+
+        setNextDTO(outdto);
+        return outdto.getForward();
+    }
+
+}

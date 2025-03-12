73c417f185f185f4d9e527229ddf3dd0597eaf5b
YukiChiba20 <yuki.chiba@takeda.com>
2025-03-12 10:32:36 +0900

    医師勤務先情報更新 修正

--- WebContent/js/ND013.js
+++ WebContent/js/ND013.js
@@ -7,65 +7,6 @@
  * @author SBS  千葉
  */
 
-/**
- * ポップアップ用ウィンドウオブジェクト
- */
-var gCseViewFromWin = null;    // 組織従業員選択(組織・担当(現))
-var gCseViewToWin = null;      // 組織従業員選択(組織・担当(新))
-var gCtcViewWin = null;        // JIS府県武田市区郡選択
-var gIiiViewWin=null;          // 施設固定コード複数入力
-var gCpcViewWin=null;          // 品目選択開始
-var gCseViewChgWin=null;       // 組織従業員選択(変更担当者)
-var gCseViewToJgiWin=null;     // 組織従業員選択(新担当者)
-var gCseViewSosWin=null;       // 組織従業員選択(組織)
-
-/**
- * <pre>
- *  全メッセージ格納用配列
- * 当画面で使用する全メッセージが格納されます。
- * </pre>
- */
-var gMsg = new Array();
-
-/**
- * <pre>
- *  画面のロード時の処理
- * 画面のロード時の処理を記述します。
- * @param msg1～5 全メッセージ
- * <pre>
- */
-function jimViewLoad(msg1,msg2,msg3,msg4,msg5, msg6){
-
-  gMsg[0] = msg1;  // 『「組織・担当(現)」及び「組織・担当(新)」が設定されていません。どちらかの指定が必須となります。』
-  gMsg[1] = msg2;  // 『施設名(全角カナ)には全角カナ文字又は全角英数字(大文字)以外は入力できません。』
-  gMsg[2] = msg3;  // 『「展開方法」が設定されていません。 』
-  gMsg[3] = msg4;  // 『新担当者を設定してください。』
-  gMsg[4] = msg5;  // 『変更内容が破棄されます。よろしいですか？』
-  gMsg[5] = msg6;  // 『登録します。よろしいですか？』
-
-  // 変更内容破棄確認メッセージの設定
-  destructMsg = gMsg[4];  // 『変更内容が破棄されます。よろしいですか？』
-
-  if (document.fm1.gamenId.value == "JKR040C010") {
-    setAllChgBtnStatus010("chgInsTrtList");
-  } else if (document.fm1.gamenId.value == "JKR040C020") {
-
-  } else if (document.fm1.gamenId.value == "JKR040C030") {
-    setAllChgBtnStatus("chgInsCityList");
-  } else if (document.fm1.gamenId.value == "JKR040C040") {
-    setAllChgBtnStatus010("chgInsTrtList");
-  }
-}
-
-/**
- * 画面のリロードやアンロードの際の処理を記述します。
- * ポップアップ画面を使用するときは使用する分を全てクリアします。
- */
-function jimUnLoad(){
-
-  jimViewClear(8);
-}
-
 
 
 /**
@@ -142,82 +83,7 @@ window.onload = function(){
 	//エラーチェックが行われている場合
 	if (document.fm1.errorCheckFlg.value != "0") {
 
-		//ダイアログメッセージを表示
-		if (dialog != "") {
 
-			if (window.confirm(dialog)) {//OK押下の場合
-
-				//チェックフラグが「勤務先追加」の場合
-				if (errorCheckFlg == "1") {
-					//ND103_医師勤務先追加
-					document.fm1.screenId.value	= "ND103";
-				    document.fm1.functionId.value = "Init"
-				}
-
-				//チェックフラグが「医師の廃業・死亡」の場合
-				if (errorCheckFlg == "2") {
-					document.fm1.screenId.value	= "NF501";
-				    document.fm1.functionId.value = "Init"
-				}
-
-				comSubmitForAnyWarp(fm1);
-
-		     }
-			 //Cancel押下の場合
-			 return false;
-		}
-
-		//チェックフラグが「勤務先追加」の場合
-		if (errorCheckFlg == "1") {
-			//ND103_医師勤務先追加
-			document.fm1.paramDocNo.value	= document.fm1.docNo.value;
-			document.fm1.screenId.value	= "ND103";
-		    document.fm1.functionId.value = "Init"
-		}
-
-		//チェックフラグが「医師の廃業・死亡」の場合
-		if (errorCheckFlg == "2") {
-			//ND105_医師削除
-			document.fm1.screenId.value	= "NF501";
-		    document.fm1.functionId.value = "Init"
-		}
-
-		if (msgStr == "") {//アクションボタン押下後エラーがない場合
-
-			//「勤務先削除」のエラーがない場合下記に遷移
-			if (errorCheckFlg == "3") {
-				//ND104_医師勤務先削除画面へ
-				document.fm1.screenId.value	= "NF501";
-			    document.fm1.functionId.value = "Init"
-			}
-
-			//「勤務先情報更新」のエラーがない場合下記に遷移
-			if (errorCheckFlg == "4") {
-				//ND101_医療機関への異動画面へ
-				document.fm1.screenId.value	= "NF501";
-			    document.fm1.functionId.value = "Init"
-			}
-
-			//「医療機関への異動」のエラーがない場合下記に遷移
-			if (errorCheckFlg == "5") {
-				//ND101_医療機関への異動画面へ
-				//異動・更新フラグを連携
-				document.fm1.screenId.value	= "NF501";
-			    document.fm1.functionId.value = "Init"
-			}
-
-			//「医療機関以外への異動」のエラーがない場合下記に遷移
-			if (errorCheckFlg == "6") {
-				//ND102_医療機関以外への異動画面へ
-				document.fm1.screenId.value	= "NF501";
-			    document.fm1.functionId.value = "Init"
-			}
-		　　
-
-		}
-
-		comSubmitForAnyWarp(fm1);
-	}
 
 }
 
@@ -277,8 +143,4 @@ function cseViewND013(td,w, callBack, winVarName, clearProc){
 	w.focus();
 	return(w);
 }
-
-
-
-
-
+}
